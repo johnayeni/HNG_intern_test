5d3f5c9b2e701a86a8640af2167bbf93106385ef
John Ayeni <john.ayeni@stu.cu.edu.ng>
2017-08-18 14:18:26 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT name, type FROM games";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["name"]. " Type: " . $row["type"]. "s<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
